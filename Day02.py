bc699951df1f611410228a12c1b5e59f22f5d040
Merarox <nnibor1@gmail.com>
2022-12-02 13:46:49 +0100

    Day02 Improvements

--- Day02.py
+++ Day02.py
@@ -1,41 +1,48 @@
 from enum import Enum
 
 class RPS(Enum):
-    A = 1 #Rock
-    B = 2 #Paper
-    C = 3 #Scissors
-    X = 1 #Rock
-    Y = 2 #Paper
-    Z = 3 #Scissors
+    ROCK = 0
+    PAPER = 1
+    Scissors = 2
 
 class Game(Enum):
-    X = 0 #Lose
-    Y = 3 #Draw
-    Z = 6 #Win
+    LOSE = 0
+    DRAW = 3
+    WIN = 6
 
+#region Enums
+def RPSEnum(input) -> RPS:
+    if(input == 'A' or input == 'X'): return RPS.ROCK
+    elif(input == 'B' or input == 'Y'): return RPS.PAPER
+    elif(input == 'C' or input == 'Z'): return RPS.Scissors
+
+def GameEnum(input) -> Game:
+    if(input == 'X'): return Game.LOSE
+    elif(input == 'Y'): return Game.DRAW
+    elif(input == 'Z'): return Game.WIN
+
+#endregion
+
+#region Calculations
 #PartOne Calculation
-def CalcScore(enemie, me) -> int:
+def CalcScore(enemy, me) -> int:
     status = 0
-    if RPS[enemie] == RPS[me]:
-        status = 3
-    elif RPS[enemie].value == 1 and RPS[me].value == 3:
-        status = 0
-    elif RPS[enemie].value == 3 and RPS[me].value == 1:
-        status = 6
-    elif RPS[enemie].value > RPS[me].value:
-        status = 0
+    if RPS(enemy) == RPS(me):
+        return Game.DRAW.value + RPS(me).value + 1
+    elif ((RPS(me).value + 2) % 3) == RPS(enemy).value:
+        return Game.WIN.value + RPS(me).value + 1
     else:
-        status = 6
-    return status + RPS[me].value
+        return Game.LOSE.value + RPS(me).value + 1
 
 #PartTwo Calculation
-def GameCalc(enemie, winStatus) -> int:
-    if(Game[winStatus] == Game.Y):
-        return RPS[enemie].value + Game[winStatus].value
-    elif(Game[winStatus] == Game.Z):
-        return (RPS[enemie].value % 3) + 1 + Game[winStatus].value
+def GameCalc(enemy, winStatus) -> int:
+    if(Game(winStatus) == Game.DRAW):
+        return RPS(enemy).value + Game(winStatus).value + 1
+    elif(Game(winStatus) == Game.WIN):
+        return ((RPS(enemy).value + 1) % 3) + Game(winStatus).value + 1
     else:
-        return RPS[enemie].value - 1 + Game[winStatus].value if RPS[enemie].value > 1 else RPS[enemie].value + 2 + Game[winStatus].value
+        return ((RPS(enemy).value + 2) % 3) + Game(winStatus).value + 1
+#endregion
 
 with open('Inputs/input02.txt', "r") as f:
     lines = f.readlines()
@@ -44,7 +51,7 @@ def PartOne():
     result = 0
     for line in lines:
         splits = line.split()
-        result += CalcScore(splits[0], splits[1])
+        result += CalcScore(RPSEnum(splits[0]), RPSEnum(splits[1]))
     print(f"PartOne: {result}")
 
 #X = Lose, Y = Draw, Z = Win
@@ -52,7 +59,7 @@ def PartTwo():
     result = 0
     for line in lines:
         splits = line.split()
-        result += GameCalc(splits[0], splits[1])
+        result += GameCalc(RPSEnum(splits[0]), GameEnum(splits[1]))
     print(f"PartTwo: {result}")
     
 PartOne()
